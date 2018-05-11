ce5a5c2c39915d9c89ebce57762daefbcf3221a8
Daniel Coutts <dcoutts95@gmail.com>
2018-05-11 17:16:07 +0100

    done

--- app/src/main/java/com/danielcoutts/goalsapp/db/daos/GoalLogDao.kt
+++ app/src/main/java/com/danielcoutts/goalsapp/db/daos/GoalLogDao.kt
@@ -3,6 +3,7 @@ package com.danielcoutts.goalsapp.db.daos
 import android.arch.persistence.room.*
 import android.arch.persistence.room.OnConflictStrategy.REPLACE
 import com.danielcoutts.goalsapp.db.entities.*
+import com.danielcoutts.goalsapp.etc.LocalDateConstants
 import com.danielcoutts.goalsapp.etc.Recurrence
 import io.reactivex.Flowable
 import org.joda.time.LocalDate
@@ -19,6 +20,9 @@ abstract class GoalLogDao {
     @Query("select * from goal_logs where goalId = :goalId and date = :date")
     abstract fun log(goalId: Long, date: LocalDate): Flowable<GoalLog>
 
+    @Query("select * from goal_logs where goalId = :goalId and date = :date")
+    abstract fun getLog(goalId: Long, date: LocalDate): GoalLog?
+
     @Insert(onConflict = REPLACE)
     abstract fun insertLog(log: GoalLog)
 
@@ -27,6 +31,22 @@ abstract class GoalLogDao {
 
     @Transaction
     open fun logForGoal(goal: Goal) {
+        val date: LocalDate = when(goal.recurrence) {
 
+            Recurrence.DAILY -> LocalDateConstants.today
+            Recurrence.WEEKLY -> LocalDateConstants.week
+            Recurrence.MONTHLY -> LocalDateConstants.month
+        }
+
+        var log = getLog(goal.id, date)
+
+        if (log == null) {
+            log = GoalLog(goal.id, goal.recurrence, date, 1)
+            insertLog(log)
+        }
+        else if (log.numberLogged < goal.target) {
+            log.numberLogged++
+            updateLog(log)
+        }
     }
 }
